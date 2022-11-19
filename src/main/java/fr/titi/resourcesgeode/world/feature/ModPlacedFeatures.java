54e2474661bfa73e802c4792b276e8efeab7db4a
Titi0603 <91018423+Titi0603@users.noreply.github.com>
2022-11-19 20:10:10 +0100

    new

--- src/main/java/fr/titi/resourcesgeode/world/feature/ModPlacedFeatures.java
+++ src/main/java/fr/titi/resourcesgeode/world/feature/ModPlacedFeatures.java
@@ -1,7 +1,6 @@
 package fr.titi.resourcesgeode.world.feature;
 
 import fr.titi.resourcesgeode.ResourceGeode;
-import fr.titi.resourcesgeode.geode.GeodeLoaderPlaced;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.placement.*;
@@ -15,18 +14,13 @@ public class ModPlacedFeatures {
     public static final DeferredRegister<PlacedFeature> PLACED_FEATURES =
             DeferredRegister.create(Registry.PLACED_FEATURE_REGISTRY, ResourceGeode.MOD_ID);
 
-    private String geodeName;
-    private int geodeOnAverageOnceEvery;
-    private int geodeAboveBottom;
-    private int geodeAbsolute;
 
-    GeodeLoaderPlaced newGeode = new GeodeLoaderPlaced("universal_geode_placed",50,6,50);
 
-    //public static final RegistryObject<PlacedFeature> UNIVERSAL_GEODE_PLACED = PLACED_FEATURES.register("universal_geode_placed",
-    //            () -> new PlacedFeature(ModConfiguredFeatures.UNIVERSAL_GEODE.getHolder().get(), List.of(
-    //                    RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(),
-    //                    HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6), VerticalAnchor.absolute(50)),
-    //                    BiomeFilter.biome())));
+    public static final RegistryObject<PlacedFeature> UNIVERSAL_GEODE_PLACED = PLACED_FEATURES.register("universal_geode_placed",
+                () -> new PlacedFeature(ModConfiguredFeatures.UNIVERSAL_GEODE.getHolder().get(), List.of(
+                        RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(),
+                        HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6), VerticalAnchor.absolute(50)),
+                        BiomeFilter.biome())));
 
 
     public static void register(IEventBus eventBus) {
