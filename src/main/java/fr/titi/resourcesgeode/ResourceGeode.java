02153cdd6cc607a2022762b1c4f65faa6f5d04a2
Titi0603 <91018423+Titi0603@users.noreply.github.com>
2022-11-19 15:16:01 +0100

    Initial commit

---
+++ src/main/java/fr/titi/resourcesgeode/ResourceGeode.java
@@ -0,0 +1,41 @@
+package fr.titi.resourcesgeode;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ResourceGeode.MOD_ID)
+public class ResourceGeode
+{
+    public static final String MOD_ID = "resourcesgeode";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ResourceGeode()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
